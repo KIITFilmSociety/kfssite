354c759b238a9d659a15a09c1e8cf0d2b23c88e4
subhadip007 <63991623+subhadip007@users.noreply.github.com>
2021-07-11 00:47:58 +0530

    Update Home.js

--- src/core/Home.js
+++ src/core/Home.js
@@ -2,7 +2,6 @@ import * as React from 'react'
 import Ellips2 from '../images/Ellips2.png'
 import Group28 from '../images/Group28.png'
 import flame from '../images/flame.png'
-<<<<<<< HEAD
 import Navbar from '../components/Navbar';
 import '../styles/Home.css';
 import { Link } from "react-router-dom";
@@ -18,23 +17,7 @@ function Home() {
     <div><img src={flame} alt="something " className="flame"/>
     <Link to="/events" style={{color:"black",textDecoration: 'none'}}><button type="button"  className="button-bot">Events</button></Link>
     </div>
-=======
-import Navbar from '../components/Navbar'
-import '../styles/Home.css'
 
-function Home () {
-  return (
-    <div className='App'>
-      <Navbar />
-      <img src={Ellips2} alt='some svg' className='elps' />
-      <img src={Group28} alt='some svg' className='grp28' />
-      <div>
-        <img src={flame} alt='something ' className='flame' />
-        <button type='button' className='button-bot'>
-					Events
-				</button>
-      </div>
->>>>>>> f4f0faf7e8e97742b7289813a1c960ce7cc322d2
     </div>
   )
 }
