a60e4bbe163d1700fa4ef8d6563e534087f74392
subhadip007 <63991623+subhadip007@users.noreply.github.com>
2021-07-11 00:45:09 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./core/Home";
@@ -7,13 +7,6 @@ import Events from "./core/Events"
 import About from './core/About'
 import Works from './core/Works'
 
-=======
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Home from './core/Home'
-import Members from './core/Members'
-import Events from './core/Events'
->>>>>>> f4f0faf7e8e97742b7289813a1c960ce7cc322d2
 
 function Routes () {
   return (
@@ -25,11 +18,7 @@ function Routes () {
       <Route path="/events" exact component={Events} />
       <Route path="/about" exact component={About} />
       <Route path="/works" exact component={Works} />
-=======
-        <Route path='/' exact component={Home} />
-        <Route path='/members' exact component={Members} />
-        <Route path='/events' exact component={Events} />
->>>>>>> f4f0faf7e8e97742b7289813a1c960ce7cc322d2
+
       </Switch>
     </BrowserRouter>
   )
