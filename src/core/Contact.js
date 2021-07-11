a1f3fea50166d04d4a4fc3b6518831f19a5236d5
subha adhikay <deathpeace007@gmail.com>
2021-07-11 11:55:38 +0530

    added contact us page

--- src/core/Contact.js
+++ src/core/Contact.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import Navbar from '../components/Navbar';
-import '../styles/works.css'
+import '../styles/contact.css'
 
 
 
@@ -9,7 +9,22 @@ return(
     <div>
     
   <Navbar/>
-  <h1>Hi</h1>
+ <div className='contact'><p className='contact-1'>
+ <h1>GET IN TOUCH</h1>
+ <h6>For any queries, you may get in touch with us :</h6>
+ <p>
+   <ul style={{'list-style': 'none','padding-left':'0'}}>
+     <li><i class="fas fa-envelope" style={{color:'red','margin-right':'3em'}}></i>filmsocietykiit@gmail.com</li>
+     <li><i class="fab fa-instagram-square"  style={{color:'red','margin-right':'3em'}}></i>filmsociety_kiit
+
+</li>
+   </ul>
+ </p>
+ <div>
+   <p className='disq'>For latest updates, join our Discord Community :</p>
+   <pre><i class="fab fa-discord" style={{color:'#4C6EF5'}}/>    <span color={{'color':'white !importent'}}>kfs</span></pre>
+ </div>
+ </p></div>
  
   <div className='works'>
     
