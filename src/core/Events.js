e730199ff2d2c0812b7369f2ad024fb29a544a75
Prabhav <prabhavtewari@gmail.com>
2021-08-05 22:08:06 +0530

    wavy event page

--- src/core/Events.js
+++ src/core/Events.js
@@ -3,6 +3,7 @@ import '../styles/events.css'
 import Navbar from '../components/Navbar1.js'
 import Image1 from '../images/kauc.jpg'
 import { Link} from 'react-router-dom'
+import Wave from 'react-wavify'
 function Events () {
   return (
     <div className='events'>
@@ -10,6 +11,7 @@ function Events () {
       <div className='container'>
         {/* <div className='row'> */}
           {/* <div className='col-md-12 m-10'> */}
+          <Link to="/eventwork">
             <div className='eventbox'>
               <img src={Image1} alt='' />
               <div className='evinfo'>
@@ -20,9 +22,21 @@ function Events () {
                 <Link to="/eventwork"><button type='button' className='btn registerbtn'>Register</button></Link>
               </div>
             </div>
+            </Link>
           {/* </div> */}
          {/* </div> */}
       </div>
+      <div className="evwave">
+      <Wave fill='#2F2F2F'
+        paused={false}
+        options={{
+          height: 20,
+          amplitude: 30,
+          speed: 0.25,
+          points: 3
+        }}
+       />
+    </div>
     </div>
   )
 }
