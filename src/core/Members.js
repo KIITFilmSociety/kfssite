48e21223c39e82e1698216303d60fa9d8a8b1dea
subha adhikay <deathpeace007@gmail.com>
2021-08-15 18:56:08 +0530

    fixed deployment issues

--- src/core/Members.js
+++ src/core/Members.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 // import React, { useState, useEffect } from 'react'
 // import firebase from './firebase'
 // import Card from '../components/Card'
